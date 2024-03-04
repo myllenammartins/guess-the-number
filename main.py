d1a1f4b6a5c2a080d23fdbb8da6dbcf31809990d
unknown <mirandamartins05@gmail.com>
2024-03-04 11:03:14 -0300

    FEAT: palpites em array, total de palpites, adicionar nome do jogador, opção de jogar novamente

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import random
 
 def guess_the_number():
-    numero_aleatorio = random.randint(1, 100)
-    tentativas = 0
-    palpite = None
 
-    while palpite != numero_aleatorio:
-        palpite = int(input("Adivinhe o número entre 1 e 100. Qual o seu palpite? "))
-        tentativas += 1
+    while True:
+        numero_aleatorio = random.randint(1, 100)
+        palpite_array = []
+        total_palpites = 0
 
-        if palpite < numero_aleatorio:
-            print("Tente um número maior")
-        elif palpite > numero_aleatorio:
-            print("Tente um número menor")
-        else:
-            print(f"UAUU, essa foi certeira. Parabéns!! Você acertou o número {numero_aleatorio} em {tentativas} tentativas.")
+        print("Bem vinda(o) ao jogo Guess the Number")
+        nome_jogador = input("Digite seu nome: ")
+
+        while True:
+            palpite = int(input(f"{nome_jogador}, qual o seu palpite? "))
+            palpite_array.append(palpite)
+            total_palpites += 1
+
+            if palpite < numero_aleatorio:
+             print("Muito baixo!")
+            elif palpite > numero_aleatorio:
+                print("Muito alto!")
+            else:
+                print(f"UAUU, essa foi certeira. Parabéns!! Você acertou o número {numero_aleatorio}")
+                print("Palpites:", end=" ")
+                for p in palpite_array:
+                    print(p, end=" ")
+                print()
+                print(f"Total de palpites: {total_palpites}")
+                break
+
+        jogar_novamente = input("Deseja jogar novamente? (s/n): ")
+        if jogar_novamente.lower() != 's':
+            print(f"Obrigado por jogar, {nome_jogador}! Até a próxima.")
+            break
 
 guess_the_number()
